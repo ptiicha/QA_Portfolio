31f0f5c798070c8de5bd68fc48dbbc5169919020
Anastasia Radachinskaia <asya070692@mail.ru>
2026-04-03 01:13:36 +0200

    add ping for Render cold start

--- tests/ui.spec.ts
+++ tests/ui.spec.ts
@@ -1,6 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('accordion works', async ({ page }) => {
+test('accordion works', async ({ page, request }) => {
+  // wake up Render (cold start)
+  for (let i = 0; i < 5; i++) {
+    const res = await request.get('https://qa-portfolio.onrender.com/topics');
+
+    if (res.status() === 200) break;
+
+    await new Promise(r => setTimeout(r, 2000));
+}
   await page.goto('https://ptiicha.github.io/QA_Portfolio/');
 
   // have to wait at least one h3 because of free Render limitation
